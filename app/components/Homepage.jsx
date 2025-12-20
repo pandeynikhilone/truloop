5a0554ccf8d725312126f2c0ba573ad6d9b56f18
Nikhil Pandey <pandeynikhilone@gmail.com>
2025-12-20 11:11:37 +0530

    feat: ProductCard && FeaturedProducts added in HomePage.jsx

--- app/components/Homepage.jsx
+++ app/components/Homepage.jsx
@@ -142,12 +142,12 @@ function HeroSection() {
 
 function OurRole() {
   return (
-    <div className="flex justify-center flex-col gap-16 my-15">
+    <div className="flex justify-center flex-col gap-12 mb-8 lg:my-15">
       <div className="text-center justify-start text-2xl md:text-3xl lg:text-5xl font-bold">
         How It Works
       </div>
 
-      {/* Fututure Update: Foreach loop have to be implemented! */}
+      {/* Fututure Update: Foreach loop have to be implemented! using array*/}
 
       <div className="md:grid md:grid-cols-2 md:gap-0 place-items-center gap-6 flex flex-col-reverse">
         {/* Text */}
@@ -185,12 +185,119 @@ function OurRole() {
   );
 }
 
+const products = [
+  {
+    id: 1,
+    name: "Xiaomi Redmi Note 15",
+    price: "20,000",
+    image: "https://placehold.co/250x250",
+    rating: 5,
+  },
+  {
+    id: 2,
+    name: "Xiaomi Redmi Note 14",
+    price: "18,500",
+    image: "https://placehold.co/250x250",
+    rating: 4,
+  },
+  {
+    id: 3,
+    name: "Xiaomi Redmi Note 13",
+    price: "16,999",
+    image: "https://placehold.co/250x250",
+    rating: 4,
+  },
+  {
+    id: 4,
+    name: "Xiaomi Redmi Note 12",
+    price: "14,999",
+    image: "https://placehold.co/250x250",
+    rating: 3,
+  },
+];
+
+function ProductCard({ product }) {
+  return (
+    <div>
+      <div className="w-72 inline-flex flex-col justify-start items-start gap-6">
+        <div className="self-stretch h-96 px-6 py-4 bg-Grey-11 rounded-3xl shadow-[1px_1px_10px_0px_rgba(204,204,204,1.00)] outline outline-4 outline-offset-[-3.50px] outline-Grey-2 flex flex-col justify-center items-center gap-4 overflow-hidden">
+          
+          <div className="flex flex-col justify-start items-center gap-4">
+            <img
+              className="w-64 h-64 rounded-[5px]"
+              src={product.image}
+              alt={product.name}
+            />
+
+            <div className="flex flex-col justify-start items-start gap-2">
+              <div className="w-60 text-Grey-2 text-base font-medium leading-5">
+                {product.name}
+              </div>
+
+              <div className="w-60 inline-flex justify-between items-center">
+                <div className="flex items-center gap-1">
+                  <div className="text-Grey-2 text-lg font-bold leading-8">
+                    {product.price}
+                  </div>
+                </div>
+
+                {/* Rating */}
+                <div className="flex">
+                  {[...Array(product.rating)].map((_, i) => (
+                    <div key={i} className="w-5 h-5 relative">
+                      <div className="w-3.5 h-3.5 absolute left-[2.81px] top-[3.07px] bg-Grey-2 outline outline-1 outline-offset-[-0.50px] outline-Grey-2" />
+                    </div>
+                  ))}
+                </div>
+
+              </div>
+            </div>
+          </div>
+        </div>
+
+        {/* Button */}
+        <div className="w-72 pl-8 pr-9 py-2.5 bg-Grey-2 rounded-[50px] outline outline-[3px] outline-offset-[-3px] outline-Grey-2 inline-flex justify-center items-center gap-2">
+          <div className="w-4 h-4 bg-Grey-11" />
+          <div className="text-Grey-11 text-base font-medium leading-5">
+            View Reviews
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+function FeaturedProducts() {
+  return (
+    <section className="flex flex-col items-center gap-16 my-16 w-full">
+      
+      {/* Heading */}
+      <div className="text-center text-2xl md:text-3xl lg:text-5xl font-bold">
+        Featured Products
+      </div>
+
+      {/* Cards Wrapper (THIS WAS MISSING) */}
+      <div className="flex flex-wrap justify-center gap-10">
+        {products.map((product) => (
+          <ProductCard
+            key={product.id}
+            product={product}
+          />
+        ))}
+      </div>
+
+    </section>
+  );
+}
+
+
 function Homepage() {
   return (
     <>
       <Navigation />
       <HeroSection />
       <OurRole />
+      <FeaturedProducts />
     </>
   );
 }
